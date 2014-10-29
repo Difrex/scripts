36d6d3f85d73fd6c0c7d636c919b700cc1cd7265
Denis(dzhel) Zheleztsov <dzhel@yandex-team.ru>
2014-10-29 17:12:11 +0300

    Makefile

--- cascade_windows/cascade.sh
+++ cascade_windows/cascade.sh
@@ -7,7 +7,7 @@ temp_wmctrl=`mktemp wmctrl.XXXXXX`
 wmctrl -l -G | awk "{if ( \$2 == $CURRENT_DESKTOP ) { print \$1, \$5, \$6; }; }" > $temp_wmctrl
 
 # Cascade windows!
-point_x=200
+point_x=250
 point_y=50
 while read w_id X Y; do
     wmctrl -i -r $w_id -e 0,$point_x,$point_y,$X,$Y
